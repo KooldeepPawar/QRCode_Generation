dcb48a5c73fc56b09fdacc44511dda6ecb6604fa
Kuldeep Pawar <164771014+KooldeepPawar@users.noreply.github.com>
2024-03-25 11:11:29 +0530

    Add files via upload

--- QRCode_Generation.py
+++ QRCode_Generation.py
@@ -2,7 +2,12 @@ import qrcode as qr
 
 link = input("Enter your URL ")
 Name = input("Enter a name for QR Code ")
-filename = (f"{Name}.png")
+# filename = (f"{Name}.png")
+filename1 = (f"c:\\c\\{Name}.png")
 
 img = qr.make(link)
-img.save(filename)
+# img.save(filename)
+img.save(filename1)
+print(filename1)
+
+# https://www.bajajfinservmarkets.in/
